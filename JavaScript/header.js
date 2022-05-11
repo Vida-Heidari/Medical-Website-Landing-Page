669e7e6c1f08c009b8ded4196aa0b86a2c0a2bbb
Vida Heidari <vidaaheidari@gmail.com>
2022-05-11 21:15:11 +0430

    header styles changed and header responsive styles added

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,18 +1,47 @@
 const navbar = document.querySelector(".navbar");
+const navBtn = document.querySelector("#nav-btn");
+
+const header = document.querySelector("#header");
 let scrolled = false;
 
+// ----------------- header --------------
 window.onscroll = function () {
   if (window.pageYOffset > 100) {
-    navbar.classList.remove("top");
+    header.classList.remove("top");
     if (!scrolled) {
-      navbar.style.transform = "translateY(-70px)";
+      header.style.transform = "translateY(-70px)";
     }
     setTimeout(function () {
-      navbar.style.transform = "translateY(0)";
+      header.style.transform = "translateY(0)";
       scrolled = true;
     }, 200);
   } else {
-    navbar.classList.add("top");
+    header.classList.add("top");
     scrolled = false;
   }
 };
+
+// ----------------- Navbar --------------
+navBtn.addEventListener("click", function () {
+  navbar.classList.add("navbar__open");
+});
+
+navbarClose.addEventListener("click", function () {
+  navbar.classList.remove("header__navbar--open");
+});
+
+// window.onscroll = function () {
+//   if (window.pageYOffset > 100) {
+//     navbar.classList.remove("top");
+//     if (!scrolled) {
+//       navbar.style.transform = "translateY(-70px)";
+//     }
+//     setTimeout(function () {
+//       navbar.style.transform = "translateY(0)";
+//       scrolled = true;
+//     }, 200);
+//   } else {
+//     navbar.classList.add("top");
+//     scrolled = false;
+//   }
+// };

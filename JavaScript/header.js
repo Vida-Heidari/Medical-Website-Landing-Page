b8e3fd6a5cb72e74df43d84c7e3c07a25dbead72
Vida Heidari <vidaaheidari@gmail.com>
2022-06-12 14:13:42 +0430

    JavaScript: header.js codes edited

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,6 +1,3 @@
-const navbar = document.querySelector(".navbar");
-const navBtn = document.querySelector("#nav-btn");
-
 const header = document.querySelector("#header");
 let scrolled = false;
 
@@ -20,28 +17,3 @@ window.onscroll = function () {
     scrolled = false;
   }
 };
-
-// ----------------- Navbar --------------
-navBtn.addEventListener("click", function () {
-  navbar.classList.add("navbar__open");
-});
-
-navbarClose.addEventListener("click", function () {
-  navbar.classList.remove("header__navbar--open");
-});
-
-// window.onscroll = function () {
-//   if (window.pageYOffset > 100) {
-//     navbar.classList.remove("top");
-//     if (!scrolled) {
-//       navbar.style.transform = "translateY(-70px)";
-//     }
-//     setTimeout(function () {
-//       navbar.style.transform = "translateY(0)";
-//       scrolled = true;
-//     }, 200);
-//   } else {
-//     navbar.classList.add("top");
-//     scrolled = false;
-//   }
-// };
